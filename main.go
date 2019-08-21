2af55860a42245b848a53c09915c2095a2381d07
m1093782566 <m1093782566@163.com>
2019-08-21 13:33:35 +0800

    change print message to hello

--- main.go
+++ main.go
@@ -12,7 +12,7 @@ func handler(w http.ResponseWriter, r *http.Request) {
 	if err != nil {
 		fmt.Fprintf(w, "Failed to get hostname, err: %v\n", err)
 	} else {
-		fmt.Fprintf(w, "Hi there, I am in %s!\n", hostName)
+		fmt.Fprintf(w, "Hello world, I am in %s!\n", hostName)
 	}
 }
 
