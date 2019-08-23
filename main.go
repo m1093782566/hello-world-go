ffb19db1eb85cd982192da8652880658b2607a6d
m1093782566 <m1093782566@163.com>
2019-08-23 18:30:27 +0800

    add hello jessie

--- main.go
+++ main.go
@@ -12,7 +12,7 @@ func handler(w http.ResponseWriter, r *http.Request) {
 	if err != nil {
 		fmt.Fprintf(w, "Failed to get hostname, err: %v\n", err)
 	} else {
-		fmt.Fprintf(w, "Hello Jack, I am in %s!\n", hostName)
+		fmt.Fprintf(w, "Hello Jessie, I am in %s!\n", hostName)
 	}
 }
 
